5b8a088d7fac61dd90aef53c039500af672888f4
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-12-24 16:27:04 +0100

    refactoring

--- croissant_llm_chat.py
+++ croissant_llm_chat.py
@@ -9,18 +9,23 @@ current_file_directory = Path(__file__).parent
 model_name = "croissantllm/CroissantLLMChat-v0.1"
 model_save_directory = Path(current_file_directory / "downloaded_models" / model_name)
 
+def load_model():
+    model_fp = model_save_directory
+
+    model = AutoModelForCausalLM.from_pretrained(model_fp, local_files_only=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_fp, local_files_only=True)
+
+    return model, tokenizer
+
 def test_llm_from_hugging_face(input: str, model, tokenizer):
-    text_generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    text_generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device=-1)
 
     llm_output = text_generation_pipeline(input, max_new_tokens=50)
 
     return llm_output[0]["generated_text"]
 
 if __name__ == "__main__":
-    model_fp = model_save_directory
-
-    model = AutoModelForCausalLM.from_pretrained(model_fp, local_files_only=True)
-    tokenizer = AutoTokenizer.from_pretrained(model_fp, local_files_only=True)
+    model, tokenizer = load_model()
 
     
     print("Interactive Mode: Type your input and press Enter. Type 'exit' to quit.")
